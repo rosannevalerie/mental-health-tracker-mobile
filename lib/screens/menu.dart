d5dfa79ef00431d185a26e5123a864a43082c3f8
Rosanne Valerie <valerierosanne@gmail.com>
2024-11-14 13:36:17 +0700

    selesai tutorial 8

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,98 +1,120 @@
 import 'package:flutter/material.dart';
 import 'package:mental_health_tracker/widgets/left_drawer.dart';
-import 'package:mental_health_tracker/screens/moodentry_form.dart'; // Impor halaman MoodEntryFormPage
 import 'package:mental_health_tracker/widgets/mood_card.dart';
 
 class MyHomePage extends StatelessWidget {
-  final String npm = '2306222986'; // NPM
-  final String name = 'Rosanne Valerie'; // Nama
-  final String className = 'PBP E'; // Kelas
+    MyHomePage({super.key});
+    final String npm = '2306222986'; // NPM
+    final String name = 'Rosanne Valerie'; // Nama
+    final String className = 'PBP E'; // Kelas
 
-  MyHomePage({super.key});
+    final List<ItemHomepage> items = [
+         ItemHomepage("Lihat Mood", Icons.mood),
+         ItemHomepage("Tambah Mood", Icons.add),
+         ItemHomepage("Logout", Icons.logout),
+    ];
 
-  final List<ItemHomepage> items = [
-    ItemHomepage("Lihat Mood", Icons.mood),
-    ItemHomepage("Tambah Mood", Icons.add),
-    ItemHomepage("Logout", Icons.logout),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    // Scaffold menyediakan struktur dasar halaman dengan AppBar dan body.
-    return Scaffold(
-      appBar: AppBar(
-        iconTheme: const IconThemeData(color: Colors.white),
-        title: const Text(
-          'Mental Health Tracker',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: Theme.of(context).colorScheme.primary,
-      ),
-      drawer: const LeftDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                InfoCard(title: 'NPM', content: npm),
-                InfoCard(title: 'Name', content: name),
-                InfoCard(title: 'Class', content: className),
-              ],
+    @override
+    Widget build(BuildContext context) {
+      // Scaffold menyediakan struktur dasar halaman dengan AppBar dan body.
+      return Scaffold(
+        // AppBar adalah bagian atas halaman yang menampilkan judul.
+        appBar: AppBar(
+          // Judul aplikasi "Mental Health Tracker" dengan teks putih dan tebal.
+          title: const Text(
+            'Mental Health Tracker',
+              style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
             ),
-            const SizedBox(height: 16.0),
-            Center(
-              child: Column(
+          ),
+          
+          // Warna latar belakang AppBar diambil dari skema warna tema aplikasi.
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          iconTheme: const IconThemeData(color: Colors.white),
+        ),
+        drawer: const LeftDrawer(),
+        // Body halaman dengan padding di sekelilingnya.
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          // Menyusun widget secara vertikal dalam sebuah kolom.
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Row untuk menampilkan 3 InfoCard secara horizontal.
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.only(top: 16.0),
-                    child: Text(
-                      'Welcome to Mental Health Tracker',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18.0,
-                      ),
-                    ),
-                  ),
-                  GridView.count(
-                    primary: true,
-                    padding: const EdgeInsets.all(20),
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                    crossAxisCount: 3,
-                    shrinkWrap: true,
-                    children: items.map((ItemHomepage item) {
-                      return ItemCard(item);
-                    }).toList(),
-                  ),
+                  InfoCard(title: 'NPM', content: npm),
+                  InfoCard(title: 'Name', content: name),
+                  InfoCard(title: 'Class', content: className),
                 ],
               ),
-            ),
-          ],
+
+              // Memberikan jarak vertikal 16 unit.
+              const SizedBox(height: 16.0),
+
+              // Menempatkan widget berikutnya di tengah halaman.
+              Center(
+                child: Column(
+                  // Menyusun teks dan grid item secara vertikal.
+
+                  children: [
+                    // Menampilkan teks sambutan dengan gaya tebal dan ukuran 18.
+                    const Padding(
+                      padding: EdgeInsets.only(top: 16.0),
+                      child: Text(
+                        'Welcome to Mental Health Tracker',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+
+                    // Grid untuk menampilkan ItemCard dalam bentuk grid 3 kolom.
+                    GridView.count(
+                      primary: true,
+                      padding: const EdgeInsets.all(20),
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      crossAxisCount: 3,
+                      // Agar grid menyesuaikan tinggi kontennya.
+                      shrinkWrap: true,
+
+                      // Menampilkan ItemCard untuk setiap item dalam list items.
+                      children: items.map((ItemHomepage item) {
+                        return ItemCard(item);
+                      }).toList(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-    );
-  }
+      );
+    }
 }
 
 class InfoCard extends StatelessWidget {
-  final String title;
-  final String content;
+  // Kartu informasi yang menampilkan title dan content.
+
+  final String title;  // Judul kartu.
+  final String content;  // Isi kartu.
 
   const InfoCard({super.key, required this.title, required this.content});
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      // Membuat kotak kartu dengan bayangan dibawahnya.
       elevation: 2.0,
       child: Container(
-        width: MediaQuery.of(context).size.width / 3.5,
+        // Mengatur ukuran dan jarak di dalam kartu.
+        width: MediaQuery.of(context).size.width / 3.5, // menyesuaikan dengan lebar device yang digunakan.
         padding: const EdgeInsets.all(16.0),
+        // Menyusun title dan content secara vertikal.
         child: Column(
           children: [
             Text(
@@ -106,64 +128,4 @@ class InfoCard extends StatelessWidget {
       ),
     );
   }
-}
-
-class ItemCard extends StatelessWidget {
-  final ItemHomepage item;
-
-  const ItemCard(this.item, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Theme.of(context).colorScheme.secondary,
-      borderRadius: BorderRadius.circular(12),
-      child: InkWell(
-        onTap: () {
-          ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("Kamu telah menekan tombol ${item.name}!")));
-
-          // Menambahkan navigasi ke halaman yang sesuai
-          if (item.name == "Tambah Mood") {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => MoodEntryFormPage(),
-              ),
-            );
-          }
-        },
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  item.icon,
-                  color: Colors.white,
-                  size: 30.0,
-                ),
-                const Padding(padding: EdgeInsets.all(3)),
-                Text(
-                  item.name,
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ItemHomepage {
-  final String name;
-  final IconData icon;
-
-  ItemHomepage(this.name, this.icon);
 }
